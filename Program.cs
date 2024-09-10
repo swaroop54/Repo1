03947cf1a9c8e9c99e63c65cdf47cb9dd35312d0
swaroop <swaroop@gmail.com>
2024-09-10 12:24:17 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class Program{
+    public static void Main(){
+        Console.WriteLine("Enter any string");
+        string n = Console.ReadLine();
+
+        char[] ch = n.ToCharArray();
+        for(int i = 0;i<ch.Length;i++)
+        {
+            int count = 1;
+            char c =ch[i];
+            ch[i]='^';
+
+            if(c!='^'){
+                for(int j=i+1;j<ch.Length;j++){
+                    if(c==ch[j]){
+                        ch[j]='^';
+                        count++;
+                    }
+                }
+                Console.WriteLine(c+" = "+count);
+            }
+        }
+ 
+        
+    }
+}
